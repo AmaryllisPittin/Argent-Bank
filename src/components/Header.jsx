c086fc648859165675a92113b190c84de5567d1e
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-04-25 09:33:48 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <nav className="main-nav">
+            <NavLink to="/" className="main-nav-logo">
+                <img className="main-nav-logo-image" src="" alt="Argent Bank Logo" />
+                <h1 className="sr-only">Argent Bank</h1>
+            </NavLink>
+            <div>
+                <NavLink className="main-nav-item" href="">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                    Sign In
+                </NavLink>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
