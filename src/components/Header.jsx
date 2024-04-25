8d631978edb1f6ba201f5a3bcd88d4ef2d4a43b6
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-04-25 10:30:49 +0200

    coupure de courant

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from "react-router-dom";
+import Logo from '../img/argentBankLogo.png';
+import SignInPage from '../pages/SignIn';
 
 const Header = () => {
     return (
         <nav className="main-nav">
             <NavLink to="/" className="main-nav-logo">
-                <img className="main-nav-logo-image" src="" alt="Argent Bank Logo" />
+                <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
                 <h1 className="sr-only">Argent Bank</h1>
             </NavLink>
             <div>
-                <NavLink className="main-nav-item" href="">
+                <NavLink to={SignInPage} className="main-nav-item">
                     <FontAwesomeIcon icon={faUserCircle} />
                     Sign In
                 </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Footer from "../components/Footer"
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Hero />
+            <Features />
+            <Footer />
         </div>
     )
 }
