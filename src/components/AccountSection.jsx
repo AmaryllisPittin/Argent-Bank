2dbde26e3de0ae00f9e4e14d6c06007f92111121
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-06-04 09:53:52 +0200

    continuation travail sur la page form, accountsection special-style à appliquer

--- src/components/AccountSection.jsx
+++ src/components/AccountSection.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-const AccountSection = () => {
+const AccountSection = ({ customClass, showIcon }) => {
     return (
-        <section className="account">
-            <div className="account-content-wrapper">
-                <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                <p className="account-amount">$2,082.79</p>
-                <p className="account-amount-description">Available Balance</p>
+        <section className={`account ${customClass}`}>
+            <div className={`account-content-wrapper ${customClass}`}>
+                <h3 className={`account-title ${customClass}`}>Argent Bank Checking (x8349)</h3>
+                <p className={`account-amount ${customClass}`}>$2,082.79</p>
+                <p className={`account-amount-description ${customClass}`}>Available Balance</p>
             </div>
-            <div className="account-content-wrapper cta">
-                <button className="transaction-button">View transactions</button>
+            <div className={`account-content-wrapper cta ${customClass}`}>
+                {showIcon ? (
+                    <FontAwesomeIcon icon={faChevronRight} className={`transaction-icon ${customClass}`} />
+                ) : (
+                    <button className={`transaction-button ${customClass}`}>View transactions</button>
+                )}
             </div>
         </section>
-    )
+    );
 }
 
 export default AccountSection;
+
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import AccountSection from "../components/AccountSection";
 import Footer from "../components/Footer"
 
 const Home = () => {
@@ -11,20 +12,25 @@ const Home = () => {
                     <form className="form">
                         <div className="form__label-input">
                             <label className="label">User name:</label>
-                            <input></input>
+                            <input className="input"></input>
                         </div>
                         <div className="form__label-input">
                             <label className="label">First name:</label>
-                            <input></input>
+                            <input className="input"></input>
                         </div>
                         <div className="form__label-input">
                             <label className="label">Last name:</label>
-                            <input></input>
+                            <input className="input"></input>
                         </div>
-                        <button>Save</button>
-                        <button>Cancel</button>
                     </form>
+                    <div className="form-button-container">
+                            <button className="form-button">Save</button>
+                            <button className="form-button">Cancel</button>
+                        </div>
                 </div>
+                <AccountSection customClass="special-style"  showIcon={true} />
+                <AccountSection customClass="special-style"  showIcon={true} />
+                <AccountSection customClass="special-style"  showIcon={true} />
             <Footer />
         </div>
     )
